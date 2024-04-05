b59a4cad64b9921adb84f532864b840643a0f926
Aleksandr Stupnikov <a.stupnikov05@gmail.com>
2024-04-05 23:59:02 +0300

    Rework Parser type completely after prototyping in kotlin

--- src/Parser/Parser.hs
+++ src/Parser/Parser.hs
@@ -26,85 +26,71 @@ import Data.Dynamic
 
 -- import Control.Monad.Trans.Cont ()
 
--- type Cont k s m a = a -> Table k s a m
--- data Entry k s a = Entry { rs :: [a], cs :: [Cont k s a a]}
--- newtype Table k s m a = Table { unTable :: State (Map.HashMap k (Map.HashMap s (Entry k s m))) [a] }
--- newtype Parser k s a = Parser { runParser :: s -> Cont k s (Dynamic, s) (a, s) -> Table k s (Dynamic, s) (a, s) }
+type St k s = Map.HashMap k (Map.HashMap s (Entry k s Dynamic))
+-- type Sta k s = State (St k s) 
+-- data Entry k s t = Entry { rs :: [t], cs :: [t -> St k s -> ([t], (St k s))]}
+data Entry k s t = Entry { rs :: [t], cs :: [t -> (St k s -> ([t], (St k s)))]}
+newtype Cont k s t = Cont { run :: forall r. (t -> (St k s -> ([r], St k s))) -> St k s -> ([r], St k s) }
+type Parser k s = StateT s (Cont k s)
 
-type Cont k s m b a = a -> Table k s m b
-data Entry k s a b m = Entry { rs :: [a], cs :: [Cont k s m b a]}
-type Table k s m b = State (Map.HashMap k (Map.HashMap s (Entry k s m b m))) [b]
-newtype Parser k s a = Parser { runParser :: forall b.
-  s -> Cont k s (Dynamic, s) (b, s) (a, s) -> Table k s (Dynamic, s) (b, s)
-}
+instance Monad (Cont k s) where
+  (>>=) :: Cont k s a -> (a -> Cont k s b) -> Cont k s b
+  (>>=) c f = Cont (\k s -> run c (\r ss -> run (f r) k ss) s)
+
+instance Functor (Cont k s) where
+  fmap :: (a -> b) -> Cont k s a -> Cont k s b
+  fmap f m = Cont (\k s -> run m (\r s1 -> k (f r) s1) s)
+
+instance Applicative (Cont k s) where
+  pure :: a -> Cont k s a
+  pure t = Cont (\k s -> k t s)
+  (<*>) :: Cont k s (a -> b) -> Cont k s a -> Cont k s b
+  (<*>) f m = Cont (\k s -> run f (\r s1 -> run (r <$> m) k s1) s)
+
+instance Alternative (Cont k s) where
+  empty :: Cont k s a
+  empty = Cont (\_ s -> ([], s))
+  (<|>) :: Cont k s a -> Cont k s a -> Cont k s a
+  (<|>) a b = Cont (\k s -> do
+      let r1 = run a k s
+      let r2 = run b k (snd r1)
+      (fst r1 <|> fst r2, snd r2)
+    )
+
+instance MonadPlus (Cont k s) where
+
+memo :: k -> Parser k s t -> Parser k s t
+memo key p = StateT (\s -> 
+    Cont (\k table ->
+        do
+          
+          return undefined
+      )
+  )
+
+-- type Result = forall r.
 
 
--- toDyn :: Cont k s (a, s) (b, s) (a, s) -> Cont k s (Dynamic, s) (b, s) (a, s)
--- toDyn c a = StateT $ \s -> c a
+-- type Cont a = ContT [a] (State (Entry a a))
+-- data Entry a b = Entry { rs :: [a], cs :: [a -> (State (Entry a a) b)]}
+-- -- newtype Table k s m a = Table { unTable :: State (Map.HashMap k (Map.HashMap s (Entry k s m))) [a] }
+-- type Parser k s = StateT s (State (Map.HashMap k (Map.HashMap s (Cont Dynamic Dynamic))))
 
--- bind :: Cont k s m a -> (a -> Cont k s m b) -> Cont k s m b
--- -- bind m f = \c -> let gg = (\x -> let ff = (f x) in ff c) in m gg
--- bind m f = \c -> m (\x -> (f x) c)
-
--- instance Functor (Parser k s) where
---   fmap :: (a -> b) -> Parser k s a -> Parser k s b
---   fmap f p = Parser (\s c -> let pp = (runParser p) s in (let xx = (c) . f in pp xx))
-
-instance Monad (Parser k s) where
-  (>>=) :: Parser k s a -> (a -> Parser k s b) -> Parser k s b
-  (>>=) (Parser p) f = Parser (\s c -> p s (\(a, s1) -> runParser (f a) s1 c))
-
-instance Functor (Parser k s) where
-  fmap :: (a -> b) -> Parser k s a -> Parser k s b
-  fmap f (Parser p) = Parser $ \s c -> p s $ \(a, s1) -> c (f a, s1)
-
-instance Applicative (Parser k s) where
-  pure :: a -> Parser k s a
-  pure x = Parser $ \s c -> c (x, s)
-  (<*>) :: Parser k s (a -> b) -> Parser k s a -> Parser k s b
-  (<*>) (Parser p1) (Parser p2) = Parser $ \s c ->
-    p1 s $ \(f, s1) ->
-    p2 s1 $ \(x, s2) ->
-    c (f x, s2)
-
--- memo :: (Hashable k, Eq k, Hashable s, Eq s, Typeable a, Typeable s) => k -> Parser k s a -> Parser k s a
--- memo key p = Parser $ \s c ->
---   do
---     modify (Map.insertWith (\_ old -> old) key Map.empty)
---     keyToParser <- get
---     let memoizedParser = keyToParser Map.! key
---     let entry = Map.lookup s memoizedParser
---     case entry of
---       Nothing -> do
---         let mem = (Entry [] [c])
---         let ff = (Map.adjust (\x -> Map.insert s mem x) key )
---         modify ff
---         return undefined
---       Just (Entry rs cs) -> undefined
---     return undefined
-    -- case Map.lookup s memoizedParser of
-    --   Nothing -> do
-    --     let !memoizedCont = memoCont $ evalParserState $ runStateT p s
-    --     -- let memoizedCont = memoCont (evalParserState $ evalStateT p s)
-    --     modify (Map.adjust (Map.insert s (toDyn <$> memoizedCont)) key)
-    --     return memoizedCont
-    --   Just memoizedCont -> return $ flip fromDyn undefined <$> memoizedCont
-
--- memoCont :: Cont k s m b a -> Cont k s m b a
--- memoCont m k = do
---   (ks, rs) <- get
+-- memoCont :: Cont a b -> Cont a b
+-- memoCont m = ContT $ \k -> do
+--   Entry ks rs <- get
+--   return undefined
 --   -- return $ evalCont m
 --   if null ks then
 --     do
---       modify $ const (k : ks, rs)
---       runCont m $ C $ \t -> do
+--       modify $ const $ Entry (k : ks) rs
+--       runContT m $ \t -> do
 --         (kss, rss) <- get
 --         modify $ const (kss, t : rss)
---         traverse_ (\kk -> runC kk t) kss
---         return t
+--         traverse_ (\kk -> kk t) kss
+--         return [t]
 --       -- return $ evalCont m 
 --   else
 --     do
 --       modify $ const (k : ks, rs)
 --       traverse (\r -> runC k r) rs
---     -- return $ evalCont m
