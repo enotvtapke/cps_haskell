1a8288ff70069ca8f16506ae46ce36de77801b75
Aleksandr Stupnikov <a.stupnikov05@gmail.com>
2024-04-12 16:19:13 +0300

    Fix Main of executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import CPS.Parser.Core
+import CPS.Parser.Memo
 import Data.Text qualified as T
 import CPS.Parser.Primitives
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  print $ _parse (single 'a' :: BaseParser k T.Text Char) "a"
-  print $ _parse (regex "[0-9]*\\.[0-9]+" :: BaseParser k T.Text T.Text) "9162.420abc"
+  print $ _parse (single 'a' :: Parser T.Text Char) "a"
+  print $ _parse (regex "[0-9]*\\.[0-9]+" :: Parser T.Text T.Text) "9162.420abc"
