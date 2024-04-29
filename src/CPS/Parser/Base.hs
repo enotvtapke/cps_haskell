ef153f1943d2d14f70577b45882a234df18fd1e4
Aleksandr Stupnikov <a.stupnikov05@gmail.com>
2024-04-29 22:24:57 +0300

    Rename types

--- src/CPS/Parser/Base.hs
+++ src/CPS/Parser/Base.hs
@@ -6,8 +6,8 @@ module CPS.Parser.Base
   )
 where
 
-import Control.Applicative (Alternative ((<|>)))
-import Control.Monad (MonadPlus, guard)
+import Control.Applicative (Alternative (empty, (<|>)))
+import Control.Monad (MonadPlus, guard, join)
 import Control.Monad.State
   ( MonadState (get),
     State,
@@ -15,19 +15,18 @@ import Control.Monad.State
     evalState,
     modify,
   )
+import Control.Monad.State.Lazy (gets)
+import Data.Bifunctor (Bifunctor (first))
 import Data.Dynamic (Dynamic (..), Typeable, fromDyn, toDyn)
 import Data.HashMap.Lazy qualified as Map
 import Data.Hashable (Hashable)
 import Data.Typeable (typeOf)
-import GHC.Base (Alternative (empty), join)
-import Control.Monad.State.Lazy (gets)
-import Data.Bifunctor (Bifunctor(first))
 
-type Table k s = Map.HashMap k (Map.HashMap s (Entry k s Dynamic Dynamic))
+type MemoTable k s = Map.HashMap k (Map.HashMap s (MemoEntry k s Dynamic Dynamic))
 
-type ContState k s = State (Table k s)
+type ContState k s = State (MemoTable k s)
 
-data Entry k s a r = Entry {rs :: [(a, s)], ks :: [(a, s) -> ContState k s [r]]}
+data MemoEntry k s a r = MemoEntry {results :: [(a, s)], continuations :: [(a, s) -> ContState k s [r]]}
 
 newtype Cont k s a = Cont {runCont :: forall r. (Typeable r) => (a -> ContState k s [r]) -> ContState k s [r]}
 
@@ -35,28 +34,28 @@ type BaseParser k s = StateT s (Cont k s)
 
 instance Monad (Cont k s) where
   (>>=) :: Cont k s a -> (a -> Cont k s b) -> Cont k s b
-  (>>=) c f = Cont (\k -> runCont c (\r -> runCont (f r) k))
+  (>>=) m f = Cont (\cont -> runCont m (\r -> runCont (f r) cont))
 
 instance Functor (Cont k s) where
   fmap :: (a -> b) -> Cont k s a -> Cont k s b
-  fmap f m = Cont (\k -> runCont m (k . f))
+  fmap f m = Cont (\cont -> runCont m (cont . f))
 
 instance Applicative (Cont k s) where
   pure :: a -> Cont k s a
-  pure t = Cont (\k -> k t)
+  pure t = Cont (\cont -> cont t)
   (<*>) :: Cont k s (a -> b) -> Cont k s a -> Cont k s b
-  (<*>) f m = Cont (\k -> runCont f (\r -> runCont (r <$> m) k))
+  (<*>) f m = Cont (\cont -> runCont f (\r -> runCont (r <$> m) cont))
 
 instance Alternative (Cont k s) where
   empty :: Cont k s a
   empty = Cont (\_ -> return empty)
   (<|>) :: Cont k s a -> Cont k s a -> Cont k s a
-  (<|>) a b =
+  (<|>) l r =
     Cont
       ( \k -> do
-          r1 <- runCont a k
-          r2 <- runCont b k
-          return $ r1 <|> r2
+          leftResults <- runCont l k
+          rightResults <- runCont r k
+          return $ leftResults <|> rightResults
       )
 
 instance MonadPlus (Cont k s)
@@ -69,23 +68,23 @@ baseMemo key parser = StateT $ \state ->
       entry <- gets (\table -> Map.lookup state $ table Map.! key)
       case entry of
         Nothing -> do
-          modify (addNewEntry state (Entry [] [toDynamicContinuation continuation]))
+          modify (addNewEntry state (MemoEntry [] [toDynamicContinuation continuation]))
           runCont
             (runStateT parser state)
             ( \result -> do
                 modify (addResult state result)
-                conts <- gets (\table -> ks $ (table Map.! key) Map.! state)
+                conts <- gets (\table -> continuations $ (table Map.! key) Map.! state)
                 join <$> mapM (\cont -> fmap fromDynamicOrError <$> cont (first toDyn result)) conts
             )
         Just foundEntry -> do
           modify (addContinuation state continuation)
-          join <$> mapM (continuation . first fromDynamicOrError) (rs foundEntry)
+          join <$> mapM (continuation . first fromDynamicOrError) (results foundEntry)
   where
     toDynamicContinuation :: (Typeable r, Typeable a) => ((a, s) -> ContState k s [r]) -> (Dynamic, s) -> ContState k s [Dynamic]
     toDynamicContinuation cont x = fmap toDyn <$> cont (first fromDynamicOrError x)
     addNewEntry state entry table = Map.insert key (Map.insert state entry (table Map.! key)) table
-    addResult state res table = Map.insert key (Map.adjust (\e -> Entry (first toDyn res : rs e) (ks e)) state (table Map.! key)) table
-    addContinuation state cont table = Map.insert key (Map.adjust (\e -> Entry (rs e) (toDynamicContinuation cont : ks e)) state (table Map.! key)) table
+    addResult state res table = Map.insert key (Map.adjust (\e -> MemoEntry (first toDyn res : results e) (continuations e)) state (table Map.! key)) table
+    addContinuation state cont table = Map.insert key (Map.adjust (\e -> MemoEntry (results e) (toDynamicContinuation cont : continuations e)) state (table Map.! key)) table
     fromDynamicOrError :: (Typeable a) => Dynamic -> a
     fromDynamicOrError dynamic = fromDyn dynamic $ error ("Dynamic has invalid type.\nGot: " <> show (typeOf dynamic))
 
@@ -96,21 +95,17 @@ baseMemo key parser = StateT $ \state ->
 -- {-# NOINLINE fromDynOrError #-}
 -- {-# SCC kToDyn #-}
 
-
 -- kToDyn :: (Typeable r, Typeable t) => (t -> ContState k s [r]) -> Dynamic -> ContState k s [Dynamic]
 -- {-# SCC kToDyn #-}
 -- kToDyn k r = (toDyn <$>) <$> {-# SCC kkkkId #-} k (fromDynOrError r)
-
 
 -- addR :: (Hashable k1, Hashable k2, Typeable a) => a -> k2 -> k1 -> Map.HashMap k1 (Map.HashMap k2 (Entry k3 s Dynamic)) -> Map.HashMap k1 (Map.HashMap k2 (Entry k3 s Dynamic))
 -- {-# SCC addR #-}
 -- addR r s key oldMap = Map.insert key (Map.adjust (\e -> Entry (toDyn r : rs e) (ks e)) s (oldMap Map.! key)) oldMap
 
-
 -- addNewEntry :: (Hashable k1, Hashable k2, Typeable r, Typeable t) => (t -> ContState k3 s [r]) -> k2 -> k1 -> Map.HashMap k1 (Map.HashMap k2 (Entry k3 s Dynamic)) -> Map.HashMap k1 (Map.HashMap k2 (Entry k3 s Dynamic))
 -- {-# SCC addNewEntry #-}
 -- addNewEntry k s key oldMap = Map.insert key (Map.insert s (Entry [] [kToDyn k]) (oldMap Map.! key)) oldMap
-
 
 -- addK :: (Hashable k1, Hashable k2, Typeable r, Typeable t) => (t -> ContState k3 s [r]) -> k2 -> k1 -> Map.HashMap k1 (Map.HashMap k2 (Entry k3 s Dynamic)) -> Map.HashMap k1 (Map.HashMap k2 (Entry k3 s Dynamic))
 -- {-# SCC addK #-}
